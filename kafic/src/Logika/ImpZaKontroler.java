d6b5e88577ae9cdfd4e1ca57b6c1bef6f7f6efa0
UrosMomcilovic <urosuki968@gmail.com>
2017-05-04 17:13:01 +0200

    Sistemski kontroler
    Napravljen konstruktor sistemskog kontrolera, impementirane neke metode

--- kafic/src/Logika/ImpZaKontroler.java
+++ kafic/src/Logika/ImpZaKontroler.java
@@ -5,14 +5,37 @@ import java.util.LinkedList;
 import javax.swing.DefaultListModel;
 import javax.swing.ListModel;
 
+import baza.bModel.KonobarBaza;
+import baza.bModel.PiceBaza;
 import interfejsi.ZaKontroler;
+import model.Konobar;
 import model.Pice;
 
 public class ImpZaKontroler implements ZaKontroler {
+	
+	private LinkedList<Pice> listaPica;
+	private LinkedList<Konobar> listaKonobara;
+	private LinkedList<Pice> prodatoPice;
+	
+	
+
+	public ImpZaKontroler() {
+		listaPica = PiceBaza.napuniListuTekucimStanjemPica();
+		listaKonobara = KonobarBaza.ubaciUListuSveKonobare();
+		prodatoPice = new LinkedList<Pice>();
+	}
 
 	public String proveriDaLiJeRegistrovan(String user, char[] password) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i = 0; i < listaKonobara.size(); i++) {
+			if(user.equals(listaKonobara.get(i).getUser())){
+				if(password.toString().equals(listaKonobara.get(i).getPass())){
+					if(listaKonobara.get(i).isAdmin())
+						return "admin";
+					else return listaKonobara.get(i).getUser();
+				}
+			}
+		}
+		return "";			
 	}
 
 	@Override
@@ -23,7 +46,11 @@ public class ImpZaKontroler implements ZaKontroler {
 
 	@Override
 	public void dodajUListuUsera(String user, String password) {
-		// TODO Auto-generated method stub
+		if(user == null || user.equals("") || user == null || user.equals(""))
+			throw new RuntimeException();
+		
+		Konobar k = new Konobar(user, password, false);
+		listaKonobara.add(k);
 
 	}
 
